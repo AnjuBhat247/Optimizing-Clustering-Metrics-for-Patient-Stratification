ed7b6b5bab620ed22587656b4d5f56812479dd94
AnjanaBhat <139755515+AnjuBhat247@users.noreply.github.com>
2024-05-31 14:46:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Clustering Algorithms',  
+    name='ClusteringAlgorithms',  
     version='0.1.0',  # Initial version number
     packages=find_packages(),
     install_requires=[
