7f7987f354b992e675e12ed3305c4c5bc88cb08e
AnjanaBhat <139755515+AnjuBhat247@users.noreply.github.com>
2024-06-04 07:16:01 +0200

    Update and rename RandomSearch.py to random_search.py

--- package/RandomSearch.py
+++
@@ -1,7 +0,0 @@
-from .utils import *
-
-def random_search(time_data, status_data,population_size=100):
-  num_patients = len(time_data)
-  population = initialize_population(population_size, num_patients)
-  scores = [logrank_fitness(cluster_indices, time_data, status_data) for cluster_indices in population]
-  return scores, population[scores.index(min(scores))]  # solution with best score
