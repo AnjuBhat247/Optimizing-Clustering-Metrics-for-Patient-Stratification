7416f65bc56511e35b2e06e1ce0b719518fac271
AnjanaBhat <139755515+AnjuBhat247@users.noreply.github.com>
2024-06-05 14:11:06 +0200

    Update random_search.py

--- package/random_search.py
+++ package/random_search.py
@@ -1,8 +1,8 @@
 from .utils import initialize_population, logrank_fitness
 
-def Random_Search(time_data, status_data,population_size=100):
+def Random_Search(time_data, status_data,population_size=100,num_clusters):
   num_patients = len(time_data)
-  population = initialize_population(population_size, num_patients)
+  population = initialize_population(population_size, num_patients,num_clusters)
   scores = [logrank_fitness(cluster_indices, time_data, status_data) for cluster_indices in population]
   best_solution = population[scores.index(min(scores))]  # solution with best score
   return scores, best_solution
