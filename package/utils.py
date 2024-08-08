4b0d8d820cd422f7000f8b3c4e740d0ff217b9aa
AnjanaBhat <139755515+AnjuBhat247@users.noreply.github.com>
2024-08-08 13:13:10 +0200

    Update utils.py

--- package/utils.py
+++ package/utils.py
@@ -13,15 +13,20 @@ def initialize_population(population_size, num_patients,num_clusters):
     population = [np.random.randint(1, num_clusters+1, num_patients) for _ in range(population_size)]  
     return population
 
-def transform_omics_data(omics_data):
+def transform_omics_data(omics_data,n_comp=None):
     transformed_omics = []
 
     for data in omics_data:
         scaler = StandardScaler()
         scaled_data = scaler.fit_transform(data)
 
-        pca = PCA(n_components='mle')
-        pca_data = pca.fit_transform(scaled_data)
+        try:
+            pca = PCA(n_components='mle')
+            pca_data = pca.fit_transform(scaled_data)
+        except ValueError as e:
+            print(f"Error with 'mle' in PCA for an omics dataset: {e}. Falling back to no n_components.")
+            pca = PCA(n_components=n_comp)  # No n_components specified, use all components
+            pca_data = pca.fit_transform(scaled_data)
 
         transformed_omics.append(pca_data)
 
@@ -29,6 +34,11 @@ def transform_omics_data(omics_data):
     final_scaler = StandardScaler()
     scaled_concatenated_data = final_scaler.fit_transform(concatenated_data)
 
-    final_pca = PCA(n_components='mle')
-    final_pca_data = final_pca.fit_transform(scaled_concatenated_data)
+    try:
+        final_pca = PCA(n_components='mle')
+        final_pca_data = final_pca.fit_transform(scaled_concatenated_data)
+    except ValueError as e:
+        print(f"Error with 'mle' in final PCA: {e}. Falling back to no n_components.")
+        final_pca = PCA(n_components=n_comp)  # No n_components specified, use all components
+        final_pca_data = final_pca.fit_transform(scaled_concatenated_data)
     return final_pca_data
